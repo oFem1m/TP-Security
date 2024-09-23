262f578099746c9a3e54e2fa6be2340aab2a3de2
Aleksandr Volokhov <volohovaleksandr1974@gmail.com>
2024-09-23 17:05:03 +0300

    init (doesnt work)

--- proxy.go
+++ proxy.go
@@ -9,12 +9,21 @@ import (
 	"net/url"
 	"strings"
 	"sync"
+	"time"
 )
 
 var requests = make([]*http.Request, 0)
 var mutex = &sync.Mutex{}
 
 func handleProxy(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodConnect {
+		handleHTTPS(w, r)
+	} else {
+		handleHTTP(w, r)
+	}
+}
+
+func handleHTTP(w http.ResponseWriter, r *http.Request) {
 	// Логируем запрос
 	mutex.Lock()
 	requests = append(requests, r)
@@ -29,14 +38,11 @@ func handleProxy(w http.ResponseWriter, r *http.Request) {
 
 	// Определяем хост
 	host := uri.Host
-	if host == "" {
-		host = r.Host
-	}
 	if !strings.Contains(host, ":") {
 		host += ":80"
 	}
 
-	// Устанавливаем соединение с сервером
+	// Устанавливаем соединение с целевым сервером
 	conn, err := net.Dial("tcp", host)
 	if err != nil {
 		http.Error(w, "Error connecting to host", http.StatusBadGateway)
@@ -45,29 +51,24 @@ func handleProxy(w http.ResponseWriter, r *http.Request) {
 	defer conn.Close()
 
 	// Перезаписываем запрос
-	requestLine := fmt.Sprintf("%s %s %s\r\n", r.Method, uri.RequestURI(), r.Proto)
+	requestLine := fmt.Sprintf("%s %s %s\\r\\n", r.Method, uri.Path, r.Proto)
 	conn.Write([]byte(requestLine))
 
 	// Передаем заголовки, удаляем "Proxy-Connection"
 	for header, values := range r.Header {
-		if strings.EqualFold(header, "Proxy-Connection") {
+		if header == "Proxy-Connection" {
 			continue
 		}
 		for _, value := range values {
-			conn.Write([]byte(fmt.Sprintf("%s: %s\r\n", header, value)))
+			conn.Write([]byte(fmt.Sprintf("%s: %s\\r\\n", header, value)))
 		}
 	}
 
-	// Убеждаемся, что заголовок "Host" установлен корректно
-	conn.Write([]byte(fmt.Sprintf("Host: %s\r\n", host)))
+	// Убедитесь, что заголовок "Host" установлен корректно
+	conn.Write([]byte(fmt.Sprintf("Host: %s\\r\\n", host)))
 
 	// Завершаем отправку заголовков
-	conn.Write([]byte("\r\n"))
-
-	// Копируем тело запроса (для POST, PUT, итд.)
-	if r.Method == "POST" || r.Method == "PUT" {
-		io.Copy(conn, r.Body)
-	}
+	conn.Write([]byte("\\r\\n"))
 
 	// Читаем ответ от сервера
 	respReader := bufio.NewReader(conn)
@@ -76,16 +77,48 @@ func handleProxy(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Error reading response", http.StatusBadGateway)
 		return
 	}
-	defer resp.Body.Close()
 
-	// Передаем ответ
+	// Передаем ответ обратно клиенту
 	for header, values := range resp.Header {
 		for _, value := range values {
 			w.Header().Add(header, value)
 		}
 	}
-
 	w.WriteHeader(resp.StatusCode)
 	io.Copy(w, resp.Body)
 	resp.Body.Close()
+}
+
+func handleHTTPS(w http.ResponseWriter, r *http.Request) {
+	// Получаем хост и порт из строки CONNECT
+	host := r.Host
+
+	// Устанавливаем соединение с целевым сервером
+	destConn, err := net.DialTimeout("tcp", host, 10*time.Second)
+	if err != nil {
+		http.Error(w, "Unable to connect to destination", http.StatusServiceUnavailable)
+		return
+	}
+	defer destConn.Close()
+
+	// Отправляем клиенту ответ о успешном установлении соединения
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, "HTTP/1.0 200 Connection established\\r\\n\\r\\n")
+
+	// Превращаем HTTP-соединение с клиентом в TCP-соединение
+	hijacker, ok := w.(http.Hijacker)
+	if !ok {
+		http.Error(w, "Hijacking not supported", http.StatusInternalServerError)
+		return
+	}
+	clientConn, _, err := hijacker.Hijack()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusServiceUnavailable)
+		return
+	}
+	defer clientConn.Close()
+
+	// Передаем данные между клиентом и сервером
+	go io.Copy(destConn, clientConn)
+	io.Copy(clientConn, destConn)
 }
